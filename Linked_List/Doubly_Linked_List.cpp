16e0ee9e5accd5369815644b2c2a40f826cab2cb
Ashish Jindal <jindalhackerrank@gmail.com>
2014-11-09 22:34:59 +0530

    DLL

--- Linked_List/Doubly_Linked_List.cpp
+++ Linked_List/Doubly_Linked_List.cpp
@@ -6,6 +6,10 @@ Inserting a new node at the middle of the list
 
 */
 
+#include<iostream>
+using namespace std;
+
+
 /* Node Structure */
 
 struct DLLNode
@@ -14,6 +18,22 @@ struct DLLNode
 	struct DLLNode *next;
 	struct DLLNode *prev;
 };
+
+
+
+/* Length Function */
+
+int DLLLength(struct DLLNode *head)
+{
+	int len=0;
+	while(head!=NULL)
+	{
+		len++;
+		head=head->next;
+	}
+	
+	return len;
+}
 
 
 /* Inserting a Node */
@@ -39,12 +59,102 @@ void DLLInsert(struct DLLNode **head,int data,int position)
 		newNode->prev=NULL;
 
 		if (*head)
-		{
-			*head->prev=newNode;
+			(*head)->prev=newNode;
+
 			*head=newNode;
 			return;
-		}
-
-	
 	}
+
+	temp=(*head);
+	while((k<position-1) && temp->next!=NULL )
+	{
+		temp=temp->next;
+		k++;
+	}
+
+	if(k!=position)
+		cout<<"Desired position does not exist"<<endl;
+
+	newNode->next=temp->next;
+	newNode->prev=temp;
+
+	if(temp->next)
+		temp->next->prev=newNode;
+
+	temp->next=newNode;
+	return;
+
 }
+
+
+
+void DLLDelete(struct DLLNode **head,int position)
+{
+	int k=1;
+	struct DLLNode *temp,*temp2;
+
+	temp=(*head);
+
+	if((*head)==NULL)
+	{
+		cout<<"List is Empty"<<endl;
+		return;
+	}
+
+	if (position==1)
+	{
+		*head=(*head)->next;
+	
+
+		if(*head!=NULL)
+			(*head)->prev=NULL;
+
+		delete(temp);
+		return;
+	}
+
+	while((k<position) && temp->next!=NULL )
+	{
+		temp=temp->next;
+		k++;
+	}
+
+	if(k!=position-1)
+		cout<<"Desired position does not exist"<<endl;
+
+	temp2=temp->prev;
+	temp2->next=temp->next;
+
+	if(temp->next)
+		temp->next->prev=temp2;
+
+	delete(temp);
+
+	return;
+}
+
+
+
+
+/*main Function */
+
+
+int main()
+{
+	int len=0;
+	struct DLLNode *head=NULL;
+
+	DLLInsert(&head,100,1)	;
+	DLLInsert(&head,200,1)	;
+	len=DLLLength(head);
+	cout<<"Length Before"<<len<<endl;
+	DLLDelete(&head,1)	;
+	len=DLLLength(head);
+	cout<<"Length After"<<len<<endl;
+
+}
+
+
+
+
+
