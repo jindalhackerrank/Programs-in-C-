059108106cb51383a401f9ec446eddeb5790c72e
Ashish Jindal <jindalhackerrank@gmail.com>
2014-11-12 08:18:53 +0530

    Loop in Linked List

--- Linked_List/Loop_in_Linked_List.cpp
+++ Linked_List/Loop_in_Linked_List.cpp
@@ -33,6 +33,41 @@ int DoesLinkedLinkedContainsLoop(struct ListNode *head)
 }
 
 
+struct ListNode* FindBeginOfLoop(struct ListNode *head)
+{
+	struct ListNode *slowPtr=head,*fastPtr=head;
+	int loopExist=0;
+
+	while(slowPtr && fastPtr && fastPtr->next)
+	{
+		slowPtr=slowPtr->next;
+		fastPtr=fastPtr->next->next;
+
+		if(slowPtr==fastPtr)
+		{
+			loopExist=1;
+			break;
+		}
+
+	}
+
+	if (loopExist)
+	{
+		slowPtr=head;
+		while(slowPtr!=fastPtr)
+		{
+			slowPtr=slowPtr->next;
+			fastPtr=fastPtr->next;
+		}
+
+		return slowPtr;
+	}
+	else
+		return NULL;
+
+}
+
+
 int main()
 {
 	return 0;
