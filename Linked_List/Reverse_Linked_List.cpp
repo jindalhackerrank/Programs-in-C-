a1cc29938d8524b93db5b92a43b62fb70febdb3b
Ashish Jindal <jindalhackerrank@gmail.com>
2014-11-16 13:33:19 +0530

    Reverse Linked List

--- Linked_List/Reverse_Linked_List.cpp
+++ Linked_List/Reverse_Linked_List.cpp
@@ -33,6 +33,37 @@ struct ListNode* RevereseLL(struct ListNode *head)
 }
 
 
+//Recursive version
+
+void recursiveReverse(struct ListNode** head_ref)
+{
+    struct ListNode* first;
+    struct ListNode* rest;
+      
+    /* empty list */
+    if (*head_ref == NULL)
+       return;  
+ 
+    /* suppose first = {1, 2, 3}, rest = {2, 3} */
+    first = *head_ref; 
+    rest  = first->next;
+ 
+    /* List has only one node */
+    if (rest == NULL)
+       return;  
+ 
+    /* reverse the rest list and put the first element at the end */
+    recursiveReverse(&rest);
+    first->next->next  = first; 
+     
+    /* tricky step -- see the diagram */
+    first->next  = NULL;         
+ 
+    /* fix the head pointer */
+    *head_ref = rest;             
+}
+
+
 int main()
 {
 	return 0;
